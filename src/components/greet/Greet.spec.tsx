aac049e4e182da9a519245cf6f732f864baaf2c2
aryanoverdose <aryan.sharma@overdose.digital>
2023-05-20 15:42:57 +0530

    different file conventions

--- src/components/greet/Greet.spec.tsx
+++ src/components/greet/Greet.spec.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
 describe("Greet", () => {
-  test("renders correctly", () => {
+  it("renders correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText("Hello");
     expect(textElement).toBeInTheDocument();
   });
 
-  test("renders with a name", () => {
+  it("renders with a name", () => {
     render(<Greet name="Aryan" />);
     const textElement = screen.getByText("Hello Aryan");
     expect(textElement).toBeInTheDocument();
